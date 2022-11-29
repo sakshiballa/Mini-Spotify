5efa1fccc97c0fe9b56ec6d9a2befa1e3a8c3a06
sakshiballa <84263933+sakshiballa@users.noreply.github.com>
2022-11-29 18:53:18 +0530

    Create PlayList.js

---
+++ components/PlayList.js
@@ -0,0 +1,11 @@
+const PlayList=[
+    {id:1,name:'Top Hits 2022'},
+    {id:2,name:'Trending'},
+    {id:3,name:'Punk'},
+    {id:4,name:'Party'},
+    {id:5,name:'Dance/Electronic'},
+    {id:6,name:'Fresh Finds'}
+
+];
+
+export{PlayList};
