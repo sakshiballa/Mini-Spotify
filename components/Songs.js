4acc4cfb3dba85675e20083f06947db037f9d367
sakshiballa <84263933+sakshiballa@users.noreply.github.com>
2022-11-29 18:54:11 +0530

    Create Songs.js

---
+++ components/Songs.js
@@ -0,0 +1,60 @@
+const Songs = [
+  {
+    id: 1,
+    favourite: false,
+    songName: "I hope we meet in another life",
+    artist: "PiKz_onthewind",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F09_-_i_hope_we_meet_in_another_life_1_.mp3?alt=media&token=76575da5-fa07-4591-9881-75a97095aeaf",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fphoto_2022-11-12_19-50-18.jpg?alt=media&token=66b93c1b-303c-413c-836d-24b2f3b0884e",
+  }, 
+  {
+      id: 2,
+      favourite: false,
+      songName: "Incompetence to Determinate",
+      artist: "PiKz_onthewind",
+      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F01%20-%20Incompetence%20to%20Determinate.wav?alt=media&token=62827ae8-7ba5-4615-acea-7c6651fa82b3",
+      imgSrc:
+        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fimg1.jpg?alt=media&token=cf7ffe32-df92-4088-8c1e-a7268400efe9",
+    },
+    {
+      id: 3,
+      favourite: false,
+      songName: "Child of the Sea",
+      artist: "PiKz_onthewind",
+      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F02-Child-of-the-Sea.mp3?alt=media&token=0f2ac1d1-475b-4bb7-a5c0-0bf67b9a4679",
+      imgSrc:
+        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fimg5.jpg?alt=media&token=1766eeaa-c4e8-414c-b341-539898fd2b0d",
+    },
+    {
+      id: 4,
+      favourite: false,
+      songName: "DON´T RUIN IT AGAIN",
+      artist: "PiKz_onthewind",
+      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F03-DON%C2%B4T-RUIN-IT-AGAIN.mp3?alt=media&token=3f7c4711-b961-43ce-b011-a2998bf5aacd",
+      imgSrc:
+        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fphoto_2022-11-12_19-50-18.jpg?alt=media&token=66b93c1b-303c-413c-836d-24b2f3b0884e",
+    },
+    {
+      id: 5,
+      favourite: false,
+      songName: "Tiger Striped Cat",
+      artist: "PiKz_onthewind",
+      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F04-Tiger-Stripped-Cat.mp3?alt=media&token=ac7193f2-413f-48d1-97ab-65e9fe760eff",
+      imgSrc:
+        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fimg4.jpg?alt=media&token=1d2c9b95-43a1-491d-9553-0ca65b9be66e",
+    },
+    {
+      id: 6,
+      favourite: false,
+      songName: "Last Dinosaur",
+      artist: "PiKz_onthewind",
+      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/songs%2F05-Last-Dinosaur.mp3?alt=media&token=3ae20957-6801-4c54-88ae-07decbbba814",
+      imgSrc:
+        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-95756.appspot.com/o/images%2Fphoto_2022-11-12_19-50-18.jpg?alt=media&token=66b93c1b-303c-413c-836d-24b2f3b0884e",
+    },
+
+    
+  ];
+  
+  export { Songs };
